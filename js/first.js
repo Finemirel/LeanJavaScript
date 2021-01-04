a7bc489ec92a9a80dc9203ce14fd6fea61bf1d99
Finemirel <finemirel@gmail.com>
2021-01-04 06:37:00 +0300

    JavaScript lesson - 1

---
+++ js/first.js
@@ -0,0 +1,15 @@
+
+//Однострочный коментарий
+var cotik = function (howManyTimes) {
+	for (var i=1; i<howManyTimes+1; i++) {
+		console.log(i+"\t='.'=");
+	}
+};
+
+cotik(12); /*Это
+мно-
+го-
+строч-
+ный
+коментарий*/
+
